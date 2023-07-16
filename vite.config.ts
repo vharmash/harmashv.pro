944baf5462d5046bfc8509433add5f4db68e062b
Volodymyr Harmash <vesper.mail@gmail.com>
2023-07-17 00:43:08 +0300

    scc and js preload

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,34 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { build, defineConfig } from 'vite'
 import { imagetools } from 'vite-imagetools'
+import VitePluginInjectPreload from 'vite-plugin-inject-preload'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    cssCodeSplit: false,
+    sourcemap: true
+  },
   plugins: [
     vue(),
-    imagetools(
-      {
-        force: true,
-      }
-    )
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+    imagetools({
+      force: true
+    }),
+    VitePluginInjectPreload({
+      files: [
+        {
+          match: /style-[a-z-0-9]*.(css)$/
+        },
+        {
+          match: /index-[a-z-0-9]*.(js)$/,
+          attributes: {
+            rel: 'modulepreload',
+            type: undefined
+          }
+        }
+      ]
+    })
+  ]
 })
